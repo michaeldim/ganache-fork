77d10e63d38f69a40b54fe475ed412d7c7abd7f9
Michael Dimmock <1141848+michaeldim@users.noreply.github.com>
2020-03-15 11:16:02 +0000

    Update run.js
    feat: move to dai token

--- run.js
+++ run.js
@@ -1,45 +1,45 @@
 const Web3 = require('web3');
-const makerAbi = require('./abi.json');
+const daiAbi = require('./abi.json');
 
 const recipientAddress = '0x0000000000000000000000000000000000000000';
 
-const unlockedAddress = '0x8EE7D9235e01e6B42345120b5d270bdB763624C7';
-const makerContractAddress = '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2';
+const unlockedAddress = '0x876EabF441B2EE5B5b0554Fd502a8E0600950cFa';
+const daiContractAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
 
 const web3 = new Web3('http://127.0.0.1:8545');
 
-const maker = new web3.eth.Contract(
-    makerAbi,
-    makerContractAddress
+const dai = new web3.eth.Contract(
+    daiAbi,
+    daiContractAddress
 );
 
 async function run() {
     let unlockedBalance, recipientBalance;
     ([unlockedBalance, recipientBalance] = await Promise.all([
-        maker.methods
+        dai.methods
             .balanceOf(unlockedAddress)
             .call(),
-        maker.methods
+        dai.methods
             .balanceOf(recipientAddress)
             .call()
     ]));
-    console.log(`Maker Balance: ${unlockedBalance}`);
+    console.log(`Dai Balance: ${unlockedBalance}`);
     console.log(`Local Account Balance: ${recipientBalance}`);
 
-    await maker.methods
+    await dai.methods
         .transfer(recipientAddress, 10)
         .send({from: unlockedAddress});
 
     ([unlockedBalance, recipientBalance] = await Promise.all([
-        maker.methods
+        dai.methods
             .balanceOf(unlockedAddress)
             .call(),
-        maker.methods
+        dai.methods
             .balanceOf(recipientAddress)
             .call()
     ]));
 
-    console.log(`New Maker Balance: ${unlockedBalance}`);
+    console.log(`New Dai Balance: ${unlockedBalance}`);
     console.log(`New Local Account Balance: ${recipientBalance}`);
 }
 
